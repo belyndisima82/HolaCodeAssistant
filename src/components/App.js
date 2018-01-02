7e86a92bba8b92acf40089d44df92aee0b2503c9
tszilard <tarszilardnorbert@gmail.com>
2018-01-02 02:01:36 +0100

    Update App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import Login from './Login'
+import Chat from './Chat'
 
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            username: '',
+            isLoggedIn: false
         }
+        this.setUsername = this.setUsername.bind(this)
+    }
+
+    setUsername(username) {
+        this.setState({
+            username,
+            isLoggedIn: true
+        })
     }
 
     render() {
-        return <h1>Chat</h1>
+        //User is not logged in, display Login component
+        if(!this.state.isLoggedIn) return <Login setUsername={this.setUsername} />
+        
+        //User is logged in, display Chat component
+        return <Chat username={this.state.username} />
     }
 }
 
