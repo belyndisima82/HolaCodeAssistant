b52a241887325463cb9d4b5786f96477028ccb2a
tszilard <tarszilardnorbert@gmail.com>
2018-03-27 10:43:03 +0200

    Add favicon

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,8 @@
 const path = require('path')
-const HtmlWebpackPlugin  = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports =  {
+module.exports = {
     entry: [
         './src/index.js'
     ],
@@ -38,6 +38,10 @@ module.exports =  {
             {
                 from: 'src/images',
                 to: 'images'
+            },
+            {
+                from: 'src/favicon.ico',
+                to: 'favicon.ico'
             }
         ])
     ]
